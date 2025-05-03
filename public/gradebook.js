819132378c7b90e921e5b5c72debdf9243beb608
pliwtcc4 <pliwtcc4@my.waketech.edu>
2025-05-02 22:43:12 -0400

    Final

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,5 +1,5 @@
 // TODO: Fetch data from the PostgreSQL database (to be implemented later)
-function fetchGradeDate() {
+function fetchGradeData() {
     // This function will query the PostgreSQL database and return grade data
     console.log("Fetching grade data...");
     // Create a new request for HTTP data
@@ -28,14 +28,14 @@ function fetchGradeDate() {
 function populateGradebook(data) {
     // This function will take the fetched grade data and populate the table
     console.log("Populating gradebook with data:", data);
-    let tableE1m = document.getElementById("gradebook"); // Get the gradebook table element
+    let tableElm = document.getElementById("gradebook"); // Get the gradebook table element
         data.forEach(function(assignment){ // For each row of data we're passed in
             let row = document.createElement("tr"); // Create a table row element
             let columns = []; // Handy place to stick the columns of information
             columns.name = document.createElement('td'); // The first column's table data will be the name
             columns.name.appendChild(
                 // Concatenate the full name: "last_name, first_name"
-                document.createTextNode(assignemtn.last_name + ", " + assignment.first_name)
+                document.createTextNode(assignment.last_name + ", " + assignment.first_name)
             );
             columns.grade = document.createElement('td'); // Second column will be the grade
             columns.grade.appendChild(
